ce462d177d72ff6e43f984cc9b9f1a6ee3042839
yusuf <122469240+yz3ro@users.noreply.github.com>
2023-11-30 22:02:38 +0300

    update

--- php/blog.php
+++ php/blog.php
@@ -1,3 +1,31 @@
+<?php
+    // Veritabanı bağlantısı
+    $conn = new mysqli("localhost", "root", "", "pawpath");
+
+    if ($conn->connect_error) {
+        die("Veritabanına bağlanırken hata: " . $conn->connect_error);
+    }
+
+    // Sayfalama işlemleri
+$postsPerPage = 4; // Her sayfada kaç blog gösterilecek
+$page = isset($_GET['page']) ? $_GET['page'] : 1; // Hangi sayfa gösteriliyor
+$startFrom = ($page - 1) * $postsPerPage; // Bu sayfa hangi sıradan başlayacak
+
+// Blog yazılarını çekme
+$sql = "SELECT * FROM blog ORDER BY created_at DESC LIMIT $startFrom, $postsPerPage";
+$result = $conn->query($sql);
+
+// Toplam sayfa sayısını bulma
+$totalPagesQuery = "SELECT COUNT(*) as total FROM blog";
+$totalPagesResult = $conn->query($totalPagesQuery);
+$totalPages = ceil($totalPagesResult->fetch_assoc()['total'] / $postsPerPage);
+
+
+
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,8 +33,46 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Pawpath</title>
         <link rel="stylesheet" href="../css/style.css">
-        <link rel="styleseet" href="../css/tasima.css">
         <script src="https://kit.fontawesome.com/b1573057bf.js" crossorigin="anonymous"></script>
+        <style>
+            .col-blog{
+                background:red;
+                padding-top: 3.5em;
+                width: calc(50% - 20px);
+            margin-bottom: 20px;
+            box-sizing: border-box;
+                padding-bottom: 3.5em;
+                
+                }
+                .col-blog1{
+                margin-top: 5rem;
+                display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+                }
+                
+                
+        .pagination {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+        .pagination a {
+            padding: 8px 16px;
+            text-decoration: none;
+            color: #007bff;
+            background-color: #fff;
+            border: 1px solid #ddd;
+            margin: 0 4px;
+            cursor: pointer;
+        }
+
+        .pagination a.active {
+            background-color: #007bff;
+            color: #fff;
+        }
+        </style>
     </head>
     <body>
         <div class="container">
@@ -48,9 +114,35 @@
             <div class="col-aboutus-top">
                 <img src="../img/blogimg.png">
             </div>
-            <div class="columns col-blog">
-           
-            </div>
+            <div class="columns col-blog1">
+            <?php
+   
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                        echo "<div class='col-blog'>";
+                        echo "<div class='col-blog-title'<h2>" . $row["title"] . "</h2></div>";
+                        $contentPreview = substr($row["content"], 0, 50); // İlk 200 karakteri al
+                        echo "<p>" . $contentPreview . "...</p>";        
+                        echo "<p class='timestamp'>Oluşturulma Tarihi: " . $row["created_at"] . "</p>";
+                        echo "</div>";
+                   
+            }
+            
+        } else {
+            echo "Henüz blog yazısı bulunmuyor.";
+        }
+        ?>
+        </div>
+    
+        <!-- Sayfalama bağlantıları -->
+        <div class="pagination">
+            <?php
+            for ($i = 1; $i <= $totalPages; $i++) {
+                echo "<a href='blog.php?page=$i'>$i</a> ";
+            }
+            ?>
+        </div>
+        
     
 
             <!--Footer-->
@@ -95,42 +187,5 @@
 
 
 
-<?php
-    // Veritabanı bağlantısı
-    $conn = new mysqli("localhost", "root", "", "pawpath");
 
-    if ($conn->connect_error) {
-        die("Veritabanına bağlanırken hata: " . $conn->connect_error);
-    }
-
-    // Sayfalama işlemleri
-$postsPerPage = 4; // Her sayfada kaç blog gösterilecek
-$page = isset($_GET['page']) ? $_GET['page'] : 1; // Hangi sayfa gösteriliyor
-$startFrom = ($page - 1) * $postsPerPage; // Bu sayfa hangi sıradan başlayacak
-
-// Blog yazılarını çekme
-$sql = "SELECT * FROM blog ORDER BY created_at DESC LIMIT $startFrom, $postsPerPage";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $title = $row['title'];
-        $content = $row['content'];
-        $image = $row['image'];
-    }
-} else {
-
-}
-// Toplam sayfa sayısını bulma
-$totalPagesQuery = "SELECT COUNT(*) as total FROM blog";
-$totalPagesResult = $conn->query($totalPagesQuery);
-$totalPages = ceil($totalPagesResult->fetch_assoc()['total'] / $postsPerPage);
-
-// Sayfalama bağlantıları
-
-for ($i = 1; $i <= $totalPages; $i++) {
-    echo "<a href='blog.php?page=$i'>$i</a> ";
-}
-$conn->close();
-?>
        
