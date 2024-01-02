4665f6c04c3664b0598b3ccabe2b546ee8ac4fda
yusuf <122469240+yz3ro@users.noreply.github.com>
2024-01-02 21:30:59 +0300

    update

--- php/user-profile.php
+++ php/user-profile.php
@@ -7,7 +7,16 @@ if (!isset($_SESSION['id'])) {
 
 $conn = new mysqli("localhost", "root", "", "pawpath");
 $conn->set_charset("utf8");
-
+$user_id = $_SESSION['id'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['gonder'])) {
+    $name = $_POST['ad'];
+    $surname = $_POST['soyisim'];
+    $mail = $_POST['mail'];
+    $stmt = $conn->prepare("UPDATE users SET name = ?,surname= ?,mail=? WHERE id = ?");
+    $stmt->bind_param('sssi', $name, $surname, $mail, $user_id);
+    $stmt->execute();
+    $stmt->close();
+}
 // $_SESSION['id'] tanımlı mı kontrolü
 $kullanici_id = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 
@@ -27,6 +36,7 @@ if ($kullanici_id !== null) {
     // $_SESSION['id'] tanımlı değilse bir hata mesajı veya başka bir işlem yapabilirsiniz.
     echo "Kullanıcı ID tanımlı değil.";
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -104,32 +114,8 @@ if ($kullanici_id !== null) {
                         <div class="prof-info-photo">
                             <img src="" alt="" />
                             <h2><?php echo $ad . ' ' . $soyad; ?></h2>
-                            <button class="button btn-profile">Profili Düzenle</button>
+                            <button class="button btn-profile" onclick="openPopup()">Profili Düzenle</button>
                         </div>
-                        <!-- <div>
-                             popup alanı 
-                            <div>
-                                <img src="" alt="" />
-                                <button>Fotoğrafı Değiştir</button>
-                            </div>
-                            <form>
-                                <div>
-                                    <div>
-                                        <label for="">İsim</label>
-                                        <input type="text" />
-                                    </div>
-                                    <div>
-                                        <label for="">Soyisim</label>
-                                        <input type="text" />
-                                    </div>
-                                    <div>
-                                        <label for="">E-mail</label>
-                                        <input type="text" />
-                                    </div>
-                                </div>
-                                <button>Kaydet</button>
-                            </form>
-                        </div>-->
                     </div>
                     <button class="button btn-go-pet">Evcil Hayvanları Görüntüle</button>
                 </div>
@@ -169,5 +155,99 @@ if ($kullanici_id !== null) {
                 </div>
             </div>
         </div>
+
+
+        <div id="popup" class="popup-container-profile">
+                     <div class="close-btn" onclick="closePopup()">X</div>
+                        <div class="col-popup-profile">
+                            <div class="col-popup-item-profile">
+                                <form action="" method="POST">
+                            <table border="0" align="center">
+                                    <tr>
+                                        <td>İsim</td>
+                                        <td>Soyisim</td>
+                                    </tr>
+                                    <tr>
+                                        <td><input type="text" name="ad" value="<?php echo $ad; ?>"></td>
+                                        <td><input type="text" name="soyisim" value="<?php echo $soyad; ?>"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>E-mail</td>
+                                        <td><input type="text" name="mail" value="<?php echo $mail; ?>"></td>
+                                    </tr>
+                             </table>
+                                    <input type="submit" name="gonder" value="Kaydet">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <script src="../js/popup.js"></script>
+        </div>
+    <script>
+    function openPopup() {
+        document.getElementById("popup").style.display = "flex";
+    }
+
+    function closePopup() {
+        document.getElementById("popup").style.display = "none";
+    }
+</script>
+
+<style>
+    .popup-container-profile {
+  display: none;
+  position: fixed;
+  width: 50%;
+  height: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding:2rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  background-color: #F0807F;
+  z-index: 1000;
+}
+.col-popup-profile{
+  width: 75%;
+  height: 250px;
+  border-radius: 1rem;
+}
+.col-popup-item-profile table {
+    color:#AC5C5B;
+    margin-right:-40px;
+    
+}
+.col-popup-item-profile input[type='submit']
+{
+    cursor: pointer ;
+  font-size: 1.5rem;
+  background-color: #AC5C5B;
+  color: white;
+  font-family: "Dongle-Bold";
+  border:none;
+  border-radius: 5px; 
+  height:2em;
+  width: 25%;
+  float: right;
+}
+.col-popup-item-profile input[type='text']
+{
+    height:2rem;
+    background-color:#FFFFFF;
+  border-radius: 10px; 
+  border-color: #AC5C5B;
+  border-width: 0 0 .5em 0;
+  border-style: solid;
+  font-family: "Coco-Sharp-Regular";
+  text-indent: 2em;
+}
+.col-popup-item-profile input[type='submit']:hover
+{
+    background: #FFFFFF;
+    color:Black;
+  transition: 0.5s;
+}
+    </style>
 </body>
 </html>
